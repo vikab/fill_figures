37c99db0e34e369a8852b4e74c86afa27c71a8df
Viktoriia <vikayo@ukr.net>
2015-05-20 11:52:54 +0300

    computer graphics lab #3

---
+++ fill figures/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace fill_figures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 397);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDoubleClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(422, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "figure";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(418, 187);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(92, 21);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Recursive";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(418, 214);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 21);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Loop";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(418, 241);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(88, 21);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Algorithm";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoEllipsis = true;
+            this.label1.Location = new System.Drawing.Point(429, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 105);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Draw the figure or click button \"figure\". Do double-click on the figure that fill" +
+    " it.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 385);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Fill figure";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ fill figures/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace fill_figures
+{
+    public partial class Form1 : Form
+    {
+        const int sizeX = 400;
+        const int sizeY = 400;
+        Bitmap _bmp = new Bitmap(sizeX,sizeY);
+        bool isDraw;
+        Pen _pen = new  Pen(Color.Black, 20);
+        Brush _brush = new SolidBrush(Color.Black);
+        Color old_color;
+
+        //int num_of_points;
+        List<Point> points = new List<Point>();
+        public Form1()
+        {
+          InitializeComponent();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDraw = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDraw = false;
+        }
+        void Drawing (Bitmap bmp, Brush brush, Point point)
+        {
+            Graphics g = Graphics.FromImage(bmp);
+            g.FillEllipse(Brushes.Black, point.X, point.Y, 20, 20);
+            
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDraw)
+            {
+                Drawing(_bmp, _brush, new Point(e.X, e.Y));
+                points.Add(new Point(e.X, e.Y));
+                pictureBox1.Image = _bmp;
+            }
+        }
+        void Brezenhem(int x1, int y1, int x2, int y2)
+        {
+            int deltaX = Math.Abs(x2 - x1);
+            int deltaY = Math.Abs(y2 - y1);
+            int signX = x1 < x2 ? 1 : -1;
+            int signY = y1 < y2 ? 1 : -1;
+            
+            int error = deltaX - deltaY;
+            Drawing(_bmp, _brush, new Point(x2, y2));
+            points.Add(new Point(x2, y2));
+            while (x1 != x2 || y1 != y2)
+            {
+                Drawing(_bmp, _brush, new Point(x1, y1));
+                points.Add(new Point(x1, y1));
+                int error2 = error * 2;
+
+                if (error2 > -deltaY)
+                {
+                    error -= deltaY;
+                    x1 += signX;
+                }
+                if (error2 < deltaX)
+                {
+                    error += deltaX;
+                    y1 += signY;
+                }
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _bmp = new Bitmap(sizeX, sizeY);
+            pictureBox1.Image = _bmp;
+           
+            Brezenhem(100, 100, 200, 100);
+            Brezenhem(200, 100, 200, 150);
+            Brezenhem(200, 150, 100, 300);
+            Brezenhem(100, 300, 100, 100);
+            pictureBox1.Image = _bmp;
+            
+        }
+
+        void Recursive(Bitmap bmp, int x, int y, Color oldColor, Color newColor)
+        {
+            if (0 <= x && x < sizeX && 0 <= y && y < sizeY && bmp.GetPixel(x, y) == oldColor)
+            {
+                bmp.SetPixel(x, y, newColor);
+                Recursive(bmp, x - 1, y, oldColor, newColor);
+                Recursive(bmp, x, y - 1, oldColor, newColor);
+                Recursive(bmp, x, y + 1, oldColor, newColor);
+                Recursive(bmp, x + 1, y, oldColor, newColor);
+            }
+
+        }
+        void Loop(Bitmap bmp, Point startPoint, Color oldColor, Color newColor)
+        {
+            List<int> x = new List<int>();
+            List<int> y = new List<int>();
+            x.Add(startPoint.X);
+            y.Add(startPoint.Y);
+
+            while (x.Count != 0)
+            {
+                if (0 <= x[0] && x[0] < sizeX && 0 <= y[0] && y[0] < sizeY && bmp.GetPixel(x[0], y[0]) == oldColor)
+                {
+                    bmp.SetPixel(x[0], y[0], newColor);
+
+                    x.Add(x[0] - 1);
+                    y.Add(y[0]);
+
+                    x.Add(x[0]);
+                    y.Add(y[0] - 1);
+
+                    x.Add(x[0]);
+                    y.Add(y[0] + 1);
+
+                    x.Add(x[0] + 1);
+                    y.Add(y[0]);
+                }
+                x.RemoveAt(0);
+                y.RemoveAt(0);
+            }
+        }
+        void Algorithm(List<Point> points)
+        {
+            for (int i = 0; i < points.Count(); i++) // i - номер текущего шага
+            {
+                int pos = i;
+                int tmp = points[i].Y;
+                Point tmp1 = points[i];
+                for (int j = i + 1; j < points.Count(); j++) 
+                {
+                    if (points[j].Y < tmp)
+                    {
+                        pos = j;
+                        tmp = points[j].Y;
+                        tmp1 = points[j];
+                    }
+                }
+                points[pos] = points[i];
+                points[i] = tmp1; 
+            }
+            for (int inx = 0; inx < points.Count; inx++)
+            {
+
+                Point buf = new Point(points[inx].X, points[inx].Y);
+                Point final_point;
+
+                for (int i = inx; i < points.Count; i++)
+                {
+                    final_point = new Point(points[i].X + 2, points[i].Y);
+                    int tmp = points[i].Y - buf.Y;
+                    if (points[i].X == buf.X + 1) break;
+                    else
+                    {
+                        if ((points[i].Y == buf.Y) || tmp < 2)
+                        {
+                            using (Graphics g = Graphics.FromImage(_bmp))
+                            {
+                                g.DrawLine(_pen, final_point, buf);
+                            }
+                        }
+                    }
+                }
+            }
+
+            points.Clear();
+        }
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            if (radioButton1.Checked)
+            {
+                old_color = _bmp.GetPixel(e.X, e.Y);
+                Recursive(_bmp, e.X, e.Y, old_color, Color.Black);
+                pictureBox1.Image = _bmp;
+                _bmp.Save("1.bmp");
+            }
+            else if (radioButton2.Checked)
+            {
+                old_color = _bmp.GetPixel(e.X, e.Y);
+                Loop(_bmp, new Point(e.X,e.Y), old_color, Color.Black);
+                pictureBox1.Image = _bmp;
+                _bmp.Save("1.bmp");
+            }
+            else if (radioButton3.Checked)
+            {
+                Algorithm(points);
+                pictureBox1.Image = _bmp;
+                _bmp.Save("1.bmp");
+            }
+            else
+            {
+                MessageBox.Show("Please, choose any algorithm!");
+            }
+        }
+
+    }
+}
